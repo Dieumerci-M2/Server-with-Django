758d94f6d693f8ed42b5e1f049e88f21b0816d5e
MUHINDO Dieumerci <dieumerci.md09@gmail.com>
2024-06-11 16:53:48 +0200

    feat : gabary loading

--- mangalab/views.py
+++ mangalab/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request) : 
-    return HttpResponse("Bienvenue dans la Librairie de Manga 😆")
+    context = {"message" : "Hello World"}
+    template = loader.get_template('mangalab/index.html')
+
+    return HttpResponse(template.render(context, request))
